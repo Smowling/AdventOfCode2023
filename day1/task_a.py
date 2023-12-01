9210dc1dbe005232c08b1fed0d0d13f9268dee6b
Daniel Smółka <smowling@gmail.com>
2023-12-01 16:55:05 +0100

    first task

---
+++ day1/task_a.py
@@ -0,0 +1,15 @@
+import re
+
+
+file = 'input.txt'
+
+result = 0
+
+with open(file) as lines:
+    for line in lines:
+        num = re.findall(r'\d', line)
+        result = result + int(num[0] + num[-1])
+
+print(result)
+
+
