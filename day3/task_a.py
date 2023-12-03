edae508c745695cf1ddb3f8fa7d43c2be5a08eca
Daniel Smółka <smowling@gmail.com>
2023-12-03 14:38:43 +0100

    day3 a

--- day3/task_a.py
+++ day3/task_a.py
@@ -3,17 +3,6 @@ import re
 with open("input.txt") as file:
     data = file.read().splitlines()
 
-test = ['467..114..',
-        '...*......',
-        '..35..633.',
-        '......#...',
-        '617*......',
-        '.....+.58.',
-        '..592.....',
-        '......755.',
-        '...$.*....',
-        '.664.598..']
-
 def check_symbol(x, x2, y, data):
     grid = [[-1, -1], [-1, 0], [-1, 1],
             [0, -1], [0,1],
@@ -23,7 +12,6 @@ def check_symbol(x, x2, y, data):
         for xx, yy in grid:
             try:
                 if not data[y+xx][p+yy].isdigit() and data[y+xx][p+yy] != '.':
-                    print(f'{y+xx}, {p+yy}: {data[y+xx][p+yy]}')
                     return True
             except:
                 pass
@@ -31,16 +19,12 @@ def check_symbol(x, x2, y, data):
 
     
 result = 0
-line_n = 0
 
-for line in data:
+for line_idx, line in enumerate(data):
     matches = re.finditer(r'\d+', line)
     for match in matches:
-        # print (f'{line_n} {match.span()} {match.group()}')
-        if check_symbol(*match.span(), line_n, data):
-            print(match.group())
+        if check_symbol(*match.span(), line_idx, data):
             result += int(match.group())
-    line_n += 1
     
 print(result)
 
