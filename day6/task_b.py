d04ff9aaf3c83aae9d20b711e81f3a41b61462e1
Daniel Smółka <smowling@gmail.com>
2023-12-06 17:19:41 +0100

    day6 bruteforce

---
+++ day6/task_b.py
@@ -0,0 +1,15 @@
+with open("input.txt") as file:
+    data = file.read().splitlines()
+
+times, distances = [list(map(int, ["".join(line.split(":")[1].split())])) for line in data]
+
+result = 1
+
+for time, distance in zip(times, distances):
+    margin = 0
+    for speed in range(time):
+        if speed * (time-speed) > distance:
+            margin += 1
+    result *= margin
+    
+print(result)
